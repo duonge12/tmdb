ffae359f66207b11971c9a70c274d5d7fa67991a
duonge17 <nhung.nguyen@digiex.group>
2025-03-01 16:17:20 +0700

    remove env

--- src/services/axios.js
+++ src/services/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 export const instance = axios.create({
   baseURL: "https://api.themoviedb.org/3",
   headers:{
-    Authorization:"Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxZDFjNGNmZDBkZTA1ZGQ4MjdiZDE3OTFiODg3YmIwNiIsIm5iZiI6MTY2NjQ5NjY3My44MjgsInN1YiI6IjYzNTRiOGExMDkyOWY2MDA3Yzk5NmYzNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.K5R8yYtgTfe4ZV_pg2MTXnaLpklismvJLS9-N9ruSyk"
+    Authorization:`Bearer ${import.meta.env.DB_API_READ_ACCESS_TOKEN}`
   }
 });
 
